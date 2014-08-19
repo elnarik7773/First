3f0764c0dfcd286c6de6efb10532524930e0d884
itisioslab <itisioslab@Mac-mini-administrator-95.local>
2014-08-19 10:36:12 +0400

    Initial Commit

---
+++ Proj/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Proj
+//
+//  Created by itisioslab on 19.08.14.
+//  Copyright (c) 2014 itisioslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
